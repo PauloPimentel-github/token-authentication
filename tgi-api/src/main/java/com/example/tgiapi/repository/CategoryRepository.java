7a7c471d72d7ec83f88fc765021cdc8c1a98a174
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2019-02-16 19:14:33 -0200

    Deletando Arquivos

--- tgi-api/src/main/java/com/example/tgiapi/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tgiapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.tgiapi.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-	
-}
